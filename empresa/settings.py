72e942987320090dc6110efee024569fdbda9750
Vecarope <vecarope@gmail.com>
2022-06-01 23:04:45 -0400

    static/bootstrap

--- empresa/settings.py
+++ empresa/settings.py
@@ -10,11 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ""))
+
+# Include BOOTSTRAP5_FOLDER in path
+BOOTSTRAP5_FOLDER = os.path.abspath(os.path.join(BASE_DIR, "..", "bootstrap5"))
+if BOOTSTRAP5_FOLDER not in sys.path:
+    sys.path.insert(0, BOOTSTRAP5_FOLDER)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -36,9 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'app',
+    'django.contrib.staticfiles', 
     'bootstrap5',
+    'app',
 ]
 
 MIDDLEWARE = [
